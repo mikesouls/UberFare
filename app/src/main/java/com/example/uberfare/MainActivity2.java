5d1f39368f966f2c5f3c69fa45dfd4c37e28b39d
z <z@thirdeye.pet>
2023-03-05 23:41:19 -0600

    finalized changes to activity 3 and UberFare

--- app/src/main/java/com/example/uberfare/MainActivity2.java
+++ app/src/main/java/com/example/uberfare/MainActivity2.java
@@ -1,9 +1,12 @@
 package com.example.uberfare;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,34 +19,56 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        Button goBack = (Button)findViewById(R.id.button2);
+        Button requestCar = (Button)findViewById(R.id.button3);
+
         SharedPreferences carPrefs = getSharedPreferences("carPrefs", MODE_PRIVATE);
         String car = carPrefs.getString("car", ""),
         distance = carPrefs.getString("distance", ""),
         measurement = carPrefs.getString("measurement", ""),
                 image = carPrefs.getString("carImage", "");
 
+        double Total = 3.00;
 
-        Toast.makeText(this, car, Toast.LENGTH_LONG);
+        // Didn't show up, maybe should be a button click?
+//        Toast.makeText(this, car, Toast.LENGTH_LONG);
 
         TextView distanceTo = (TextView)findViewById(R.id.distanceToTravel),
                 carOrder = (TextView)findViewById(R.id.requestCar),
                 totalCost = (TextView)findViewById(R.id.totalCost);
         ImageView carImage = (ImageView) findViewById(R.id.carImage);
 
-        distanceTo.setText(" "+distance+" "+measurement);
-        carOrder.setText(" "+car);
-
-
         if(car.equals("Smart Car")){
             carImage.setImageResource(R.drawable.smartcar);
+            Total += 2.00;
         } else if (car.equals("Sedan")) {
             carImage.setImageResource(R.drawable.whatisasedan);
         }
         else {
             carImage.setImageResource(R.drawable.minivan);
+            Total += 5.00;
         }
 
-        // TODO: IMPLEMENT THE MATH CALCULATIONS TO GET TOTAL
+        double parsedMileage = Double.parseDouble(distance.toString());
+        Total += (parsedMileage * 3.25);
+
+        distanceTo.setText(" "+distance+" "+measurement);
+        carOrder.setText(" "+car);
+        totalCost.setText(String.format(" %.2f", Total));
+
+        goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this, MainActivity.class));
+            }
+        });
+        requestCar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity2.this, MainActivity3.class));
+            }
+        });
+
 
         // distanceTo.setText();
 
--- app/src/main/java/com/example/uberfare/MainActivity3.java
+++ app/src/main/java/com/example/uberfare/MainActivity3.java
@@ -1,12 +1,27 @@
 package com.example.uberfare;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Random;
+
 public class MainActivity3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        TextView driver = (TextView)findViewById(R.id.driverBox);
+
+        String[] name = {"Jeremy Craig", "Lawrence Douglas", "Tucker Carlson", "Jerry Springer", "Harry Jacobs"};
+        int nameLength = name.length;
+        Random random = new Random();
+        driver.setText(String.format("Your driver %s is on their way", name[random.nextInt(nameLength)]));
+
+        TextView randomTime = (TextView)findViewById(R.id.randomETA);
+        randomTime.setText(String.format("Possible Estimated Time of Arrival:\n %d Minutes", random.nextInt(60)));
 
     }
 }
